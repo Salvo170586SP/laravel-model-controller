7f5aea56f394edb1e357e037e1d93dbfe2ae43ff
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-17 15:59:41 +0100

    added foreach card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,24 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My movie</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
-    <h1>Movie</h1>
-
-    @forelse($movies as $movie)
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title h2">{{$movie->title}}</h5>
-            <h6 class="card-subtitle mb-2 text-muted h3">{{ $movie->original_title }}</h6>
-            <p class="card-text">{{ $movie->nationality }}</p>
-            <p class="card-text">{{ $movie->date }}</p>
-            <p class="card-text">{{ $movie->vote }}</p>
+    <div class="container">
+        <h1>Movie</h1>
+        <div class="row">
+            
+            @forelse($movies as $movie)
+                <div class="card col-5" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title h2">Titolo: {{$movie->title}}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted h3">Titolo originale: {{ $movie->original_title }}</h6>
+                        <p class="card-text">Nazione: {{ $movie->nationality }}</p>
+                        <p class="card-text">Data di uscita: {{ $movie->date }}</p>
+                        <p class="card-text">Voto: {{ $movie->vote }}</p>
+                    </div>
+                </div>
+                @empty
+                <h2>NON CI SONO MOVIE DISPONIBILI</h2>
+                @endforelse
         </div>
     </div>
-    @empty
-    <h2>NON CI SONO MOVIE DISPONIBILI</h2>
-    @endforelse
 </body>
 
 </html>
