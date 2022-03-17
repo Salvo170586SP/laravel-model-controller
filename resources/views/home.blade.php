d6f873ae5fc9a913d5c3c639a91fc4c83a264d92
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-17 15:46:09 +0100

    added forelse card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>My movie</title>
 </head>
+
 <body>
     <h1>Movie</h1>
+
+    @forelse($movies as $movie)
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title h2">{{$movie->title}}</h5>
+            <h6 class="card-subtitle mb-2 text-muted h3">{{ $movie->original_title }}</h6>
+            <p class="card-text">{{ $movie->nationality }}</p>
+            <p class="card-text">{{ $movie->date }}</p>
+            <p class="card-text">{{ $movie->vote }}</p>
+        </div>
+    </div>
+    @empty
+    <h2>NON CI SONO MOVIE DISPONIBILI</h2>
+    @endforelse
 </body>
+
 </html>
